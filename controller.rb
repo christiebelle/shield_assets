7cbfd6acd6f71a4d8c256136c9dc7e6a5c654ceb
Alan Russell <ajrussellaudio@gmail.com>
2018-02-14 14:32:10 +0000

    add new view

--- controller.rb
+++ controller.rb
@@ -7,3 +7,13 @@ get "/heroes" do
   @heroes = Hero.all()
   erb(:index)
 end
+
+get "/heroes/new" do
+  erb(:new)
+end
+
+post "/heroes/new" do
+  new_hero = Hero.new(params)
+  new_hero.save()
+  redirect to "/heroes"
+end
