aa468ba5bef56c03e826cbc8baa3b70e6dc4d1f4
Christiebelle <cslester@outlook.com>
2018-02-16 18:57:13 +0000

    css added

--- controller.rb
+++ controller.rb
@@ -3,6 +3,7 @@ require("sinatra/contrib/all")
 
 require_relative("./models/Hero")
 require_relative("./models/Squad")
+require_relative("./models/Villain")
 
 get "/" do
   erb(:home)
@@ -17,15 +18,31 @@ get "/heroes" do
   erb(:index)
 end
 
+get "/villains" do
+  @villains = Villain.all()
+  erb(:villain_index)
+end
+
 get "/heroes/new" do
   @squads = Squad.all()
   erb(:new)
+end
+
+get "/villains/new" do
+  @squads = Squad.all()
+  erb(:villain_new)
 end
 
 post "/heroes/new" do
   new_hero = Hero.new(params)
   new_hero.save()
   redirect to "/heroes"
+end
+
+post "/villains/new" do
+  new_villain = Villain.new(params)
+  new_villain.save()
+  redirect to "/villains"
 end
 
 get "/squads" do
@@ -45,4 +62,12 @@ end
 
 get "/login" do
   erb(:login)
+end
+
+get "/contact" do
+  erb(:contact)
+end
+
+get "/jobs" do
+  erb(:jobs)
 end
